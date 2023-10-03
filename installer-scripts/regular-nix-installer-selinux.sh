337f55d79c260ca69851ad28f0493fc274890d4f
Matthew Conway <matthew.conway@regeneron.com>
2023-10-03 16:29:20 +0000

    use curl everywhere, change to MC-Escherichia

--- installer-scripts/regular-nix-installer-selinux.sh
+++ installer-scripts/regular-nix-installer-selinux.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-if ! command -v wget &> /dev/null
-then
-    echo "wget is required for this script to work"
-    exit 2
-fi
-
 sudo sleep 2
 echo "Adding selinux content to /nix"
 sudo semanage fcontext -a -t etc_t '/nix/store/[^/]+/etc(/.*)?' ; sudo semanage fcontext -a -t lib_t '/nix/store/[^/]+/lib(/.*)?' ; sudo semanage fcontext -a -t systemd_unit_file_t '/nix/store/[^/]+/lib/systemd/system(/.*)?' ; sudo semanage fcontext -a -t man_t '/nix/store/[^/]+/man(/.*)?' ; sudo semanage fcontext -a -t bin_t '/nix/store/[^/]+/s?bin(/.*)?' ; sudo semanage fcontext -a -t usr_t '/nix/store/[^/]+/share(/.*)?' ; sudo semanage fcontext -a -t var_run_t '/nix/var/nix/daemon-socket(/.*)?' ; sudo semanage fcontext -a -t usr_t '/nix/var/nix/profiles(/per-user/[^/]+)?/[^/]+'
@@ -52,21 +46,25 @@ sudo setenforce Enforcing
 
 sleep 1
 
-bash <(curl -s https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/other-scripts/nix-sudo-path.sh)
+bash <(curl -s https://raw.githubusercontent.com/MC-Escherichia/nix-installer-scripts/main/other-scripts/nix-sudo-path.sh)
 
 echo "Modifying configurations"
 
 sleep 1
 
-sudo rm -f /etc/nix/nix.conf ; sudo wget -P /etc/nix https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/other-files/nix.conf
-sudo rm -f /etc/profile.d/nix-app-icons.sh ; sudo wget -P /etc/profile.d https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/other-files/nix-app-icons.sh
+sudo rm -f /etc/nix/nix.conf ;
+curl /etc/nix https://raw.githubusercontent.com/MC-Escherichia/nix-installer-scripts/main/other-files/nix.conf > /tmp/nix.conf
+sudo mv /tmp/nix.conf /etc/nix/nix.conf
+sudo rm -f /etc/profile.d/nix-app-icons.sh
+sudo curl https://raw.githubusercontent.com/MC-Escherichia/nix-installer-scripts/main/other-files/nix-app-icons.sh > /tmp/nix-app-icons.sh
+sudo mv /tmp/nix-app-icons.sh /etc/profile.d/nix-app-icons.sh
 
 
 sudo echo "Building nix package manager..."
 
 sleep 1
 
-curl -s https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/nix-out-of-default/setup.sh | bash -s /usr
+#curl -s https://raw.githubusercontent.com/MC-Escherichia/nix-installer-scripts/main/nix-out-of-default/setup.sh | bash -s /usr
 
 sudo echo "Cleaning up..."
 
@@ -86,7 +84,7 @@ echo "Making a nix backup"
 
 sleep 1
 
-bash <(curl -s https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/backup-scripts/create-backup-selinux.sh)
+#bash <(curl -s https://raw.githubusercontent.com/MC-Escherichia/nix-installer-scripts/main/backup-scripts/create-backup-selinux.sh)
 
 echo "Reboot your system by typing"
 echo "systemctl reboot"
